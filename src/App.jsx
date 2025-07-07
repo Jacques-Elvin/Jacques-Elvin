9cb02cd42057bd1ca13feeec3466819060f2e04c
Jacques Elvin <jxselvin1@gmail.com>
2025-07-07 17:03:24 +0530

    import

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 import Technologies from "./components/Technologies";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 
 function App() {
   return (
